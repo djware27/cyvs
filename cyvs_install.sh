074313df02912cfd205647fff5658300ff6867da
Donald Ware <djware@gmail.com>
2025-10-17 06:50:28 -0500

    Add files via upload
    Initial updload of cyvs_install.sh

---
+++ cyvs_install.sh
@@ -0,0 +1,3 @@
+ sudo grype db update    # update the CVE database
+ curl -sSfL https://get.anchore.io/grype | sudo sh -s -- -b /usr/local/bin
+ curl -sSfL https://get.anchore.io/syft | sudo sh -s -- -b /usr/local/bin
